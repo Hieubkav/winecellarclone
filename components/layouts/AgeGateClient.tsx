35187c3b2a39df671c067acf4cec9b6370cb2663
Noah Hill <noahhill576@aol.com>
2026-03-29 16:24:45 +0700

    frontend: agegate client mount guard

--- components/layouts/AgeGateClient.tsx
+++ components/layouts/AgeGateClient.tsx
@@ -1,11 +1,14 @@
 "use client";
 
-import dynamic from "next/dynamic";
-
-const AgeGate = dynamic(() => import("@/components/layouts/AgeGate"), {
-  ssr: false,
-});
+import AgeGate from "@/components/layouts/AgeGate";
+import { useHydrated } from "@/hooks/use-hydrated";
 
 export default function AgeGateClient() {
+  const hydrated = useHydrated();
+
+  if (!hydrated) {
+    return null;
+  }
+
   return <AgeGate />;
 }
